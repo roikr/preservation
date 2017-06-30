d1b2dafce931fc49bc0a0473f9940852db05e6f7
Roee Kremer <roikr75@gmail.com>
2017-06-30 12:47:17 +0300

    preservation for linux

--- preservation/src/main.cpp
+++ preservation/src/main.cpp
@@ -3,13 +3,14 @@
 
 //========================================================================
 int main( ){
-    ofGLWindowSettings settings;
-    settings.glVersionMinor=3;
-    settings.glVersionMajor=3;
-    settings.width = 1280;
-    settings.height=800;
+    // ofGLWindowSettings settings;
+    // settings.glVersionMajor=4;
+    // settings.glVersionMinor=3;
+    // settings.width = 1280;
+    // settings.height=800;
     
-    ofCreateWindow(settings);
+    // ofCreateWindow(settings);
+    ofSetupOpenGL(1280,800,OF_WINDOW);
 	ofRunApp(new ofApp());
 
 }
