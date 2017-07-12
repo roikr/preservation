9b2e990885797bdabb5aaaf9593a0c074e15b58f
Roee Kremer <roikr75@gmail.com>
2017-07-12 13:56:13 +0300

    forestProto - simplify coordinate conversions

--- forestProto/src/ofApp.cpp
+++ forestProto/src/ofApp.cpp
@@ -6,8 +6,10 @@
 float of2b(float x) {return x/BOX2D_TO_OF_SCALE;}
 float b2of(float x) {return x*BOX2D_TO_OF_SCALE;}
 
-b2Vec2 of2b(ofVec2f v) {return b2Vec2(v.x/BOX2D_TO_OF_SCALE,(ofGetHeight()-v.y)/BOX2D_TO_OF_SCALE);}
-ofVec2f b2of(b2Vec2 v) {return ofVec2f(v.x*BOX2D_TO_OF_SCALE,ofGetHeight()-v.y*BOX2D_TO_OF_SCALE);}
+b2Vec2 of2b(ofVec2f v) {return 1/BOX2D_TO_OF_SCALE*b2Vec2(v.x,v.y);}
+ofVec2f b2of(b2Vec2 v) {return BOX2D_TO_OF_SCALE*ofVec2f(v.x,v.y);}
+
+ofVec2f touchToWorld(float x,float y) {return ofVec2f(x,ofGetHeight()-y);}
 
 enum {
     TYPE_GROUND,
@@ -17,10 +19,10 @@ enum {
 
 vector<ofVec2f> createBox(float width,float height) {
     vector<ofVec2f> verts;
-    verts.push_back(0.5*ofVec2f(-width,height));
-    verts.push_back(0.5*ofVec2f(width,height));
-    verts.push_back(0.5*ofVec2f(width,-height));
     verts.push_back(0.5*ofVec2f(-width,-height));
+    verts.push_back(0.5*ofVec2f(width,-height));
+    verts.push_back(0.5*ofVec2f(width,height));
+    verts.push_back(0.5*ofVec2f(-width,height));
     return verts;
 }
 
@@ -58,7 +60,7 @@ instance::instance(b2World *world,int type,ofVec2f pos,vector<ofVec2f> poly):pol
     for (int i=0;i<poly.size();i++) {
         b2Vec2 verts[3];
         vector<ofVec2f> vec{ofVec2f(0),poly[i],poly[(i+1)%poly.size()]};
-        for (int j=0;j<3;j++) verts[j].Set(of2b(vec[j].x),-of2b(vec[j].y));
+        for (int j=0;j<3;j++) verts[j]=of2b(vec[j]);
         b2PolygonShape tri;
         tri.Set(verts, 3);
         b2FixtureDef fixture;
@@ -106,7 +108,7 @@ void ofApp::setup(){
     m_world->SetContactListener(this);
     
     //instances.push_back(make_shared<instance>(m_world,TYPE_GROUND,false,createBox(ofGetWidth(),50),ofVec2f(ofGetWidth()/2,ofGetHeight())));
-    instances.push_back(make_shared<instance>(m_world,TYPE_GROUND,ofVec2f(ofGetWidth()/2,ofGetHeight()),ofGetWidth(),10));
+    instances.push_back(make_shared<instance>(m_world,TYPE_GROUND,ofVec2f(ofGetWidth()/2,0),ofGetWidth(),10));
 }
 
 //--------------------------------------------------------------
@@ -119,6 +121,9 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    ofPushMatrix();
+    ofTranslate(0, ofGetHeight());
+    ofScale(1,-1,1);
     ofSetColor(counter ? ofColor::red : ofColor::white);
     for (auto &i:instances) {
         ofPushMatrix();
@@ -131,6 +136,7 @@ void ofApp::draw(){
         ofEndShape(true);
         ofPopMatrix();
     }
+    ofPopMatrix();
 }
 
 void ofApp::exit(){
@@ -172,6 +178,8 @@ void ofApp::mouseMoved(int x, int y ){
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
+    ofVec2f pos=touchToWorld(x,y);
+    
     switch (button) {
         case 1: {
             auto it = find_if(instances.begin(), instances.end(),[](shared_ptr<instance> p) { return p->type==TYPE_USER;});
@@ -179,22 +187,24 @@ void ofApp::mouseDragged(int x, int y, int button){
                 m_world->DestroyBody((*it)->body);
                 instances.erase(it);
             }
-            instances.push_back(make_shared<instance>(m_world,TYPE_USER,ofVec2f(x,y),400, 40));
+            instances.push_back(make_shared<instance>(m_world,TYPE_USER,pos,400, 40));
         }  break;
     }
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
+    ofVec2f pos=touchToWorld(x,y);
+    
     switch (button) {
         case 0:
-            instances.push_back(make_shared<instance>(m_world,TYPE_POLY,ofVec2f(x,y),30, 70));
+            instances.push_back(make_shared<instance>(m_world,TYPE_POLY,pos,30, 70));
             break;
         case 1:
-            instances.push_back(make_shared<instance>(m_world,TYPE_USER,ofVec2f(x,y),400, 40));
+            instances.push_back(make_shared<instance>(m_world,TYPE_USER,pos,400, 40));
             break;
         case 2:
-            instances.push_back(make_shared<instance>(m_world,TYPE_POLY,ofVec2f(x,y),createRandomPoly(50, 100)));
+            instances.push_back(make_shared<instance>(m_world,TYPE_POLY,pos,createRandomPoly(50, 100)));
             break;
     }
 }
